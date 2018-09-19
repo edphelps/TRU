f5615ea0eb0c04e5ac1888cbc89c147371d26e60
Ed Phelps <edphelps1@gmail.com>
2018-09-19 10:15:24 -0600

    Basic functionality complete
    Still need to improve home page, banner iimage, and prevent multiple asynch requests.

--- app.js
+++ app.js
@@ -800,35 +800,112 @@ function onMenuVolunteerAssignments() {
     }); // catch
 }
 
+/* *******************************************************
+* getDurationHours(dtDuration)
+* Helper to get hours from a duraction entered into the docs sheet.
+*  @param (date) dtDuration - a date for a duration.  These start on
+*                             12/30/1899 00:00:00 for some reason.
+* return (float) - number of decimal hours represented by the duration
+******************************************************* */
+function getDurationHours(dtDuration) {
+  return (dtDuration.getTime() / 1000 / 60 / 60) + 613649;
+}
+/* *******************************************************
+*  getDateOnly()
+*  Helper to get just the date for display
+*  @param dt (Date) - a date
+* return (string) - "01/06/1998"
+******************************************************* */
+function getDateOnly(dt) {
+  dt = new Date(dt);  // this allows the dt param to be Date or String
+  if (isNaN(dt))
+    return "?";
+  return dt.getMonth()+1 + "/" + dt.getDate() + "/" + dt.getFullYear();
+}
+
 /* ==================================================
-*  getVolunteerStats()
+*  getVolunteerStatsDocs()
 *
 *  @param sVolunteerDocs (doc objects)
 *  @return elem with all the stats setup for display
 * =================================================== */
-function getVolunteerStats(aDocs) {
-  /* *******************************************************
-  * getDurationHours(dtDuration)
-  * Helper to get hours from a duraction entered into the docs sheet.
-  *  @param (date) dtDuration - a date for a duration.  These start on
-  *                             12/30/1899 00:00:00 for some reason.
-  * return (float) - number of decimal hours represented by the duration
-  ******************************************************* */
-  function getDurationHours(dtDuration) {
-    return (dtDuration.getTime() / 1000 / 60 / 60) + 613649;
+function getVolunteerStatsDocs(aDocs) {
+
+  const elemContainer = document.createElement('div');
+
+  // filter for service date in last 90 days
+  const dt90DaysAgo = new Date(new Date() - 90 * MILLISEC_IN_A_DAY);
+  const aDocs90Days = aDocs.filter(oDoc => dt90DaysAgo <= oDoc["Date of service"]);
+
+  // sort assignments from last 90 days by date of service 
+  aDocs90Days.sort((oDoc1, oDoc2) => {
+    if (oDoc1["Date of service"] < oDoc2["Date of service"])
+      return -1;
+    if (oDoc2["Date of service"] < oDoc1["Date of service"])
+      return 1;
+    return 0;
+  });
+
+
+  // SHOW RECENT DOCS
+  // =====================================
+
+  // add a title
+  elemHeading = document.createElement('H4');
+  elemHeading.classList.add("ml-2", "mt-4", "text-center");
+  elemHeading.innerText = "My Recent Docs";
+  elemContainer.appendChild(elemHeading);
+
+
+  // create the table to display the assignments
+  elemTable = document.createElement('table');
+  elemTable.classList.add("table", "table-sm", "table-striped");
+  // thead
+  elemTHead = document.createElement('thead');
+  elemTHead.classList.add("bg-info", "text-light");
+  elemTHead.appendChild(makeRowHeading("Svc Dt", "Patient", "Care Plan", "Service", "Hrs", "Miles", "Calls"));
+  elemTable.appendChild(elemTHead);
+  // tbody
+  elemTBody = document.createElement('tbody');
+  // rows
+  for (const oDoc of aDocs90Days) {
+
+    let fHours = getDurationHours(new Date(oDoc["Total time spent"]));
+    // oTotals.fHours.toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 }),
+    // oTotals.fMiles.toLocaleString(undefined, { maximumFractionDigits: 0 });
+    // FIX
+
+    let elemRow = makeRow(
+      getDateOnly(oDoc["Date of service"]),
+      oDoc["Patient name"],
+      oDoc["Care Plan"],
+      oDoc["What did you do?"],
+      // getDurationHours(new Date(oDoc["Total time spent"])),
+      fHours.toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 }),
+      oDoc.Mileage,
+      oDoc["Number of calls, if any"],
+    );
+
+    elemTBody.appendChild(elemRow);
+    elemRow = makeSpanningRow(oDoc["Notes"], 7);
+    elemTBody.appendChild(elemRow);
   }
-  /* *******************************************************
-  *  getDateOnly()
-  *  Helper to get just the date for display
-  *  @param dt (Date) - a date
-  * return (string) - "01/06/1998"
-  ******************************************************* */
-  function getDateOnly(dt) {
-    dt = new Date(dt);  // this allows the dt param to be Date or String
-    if (isNaN(dt))
-      return "?";
-    return dt.getMonth()+1 + "/" + dt.getDate() + "/" + dt.getFullYear();
-  }
+  elemTable.appendChild(elemTBody);
+  // add to the container
+  elemContainer.appendChild(elemTable);
+
+  return elemContainer;
+}
+
+/* ==================================================
+*  getVolunteerStatsAnnual()
+*
+*  Return container element with the annual totals in a table
+*
+*  @param sVolunteerDocs (doc objects)
+*  @return elem with all the stats setup for display
+* =================================================== */
+function getVolunteerStatsAnnual(aDocs) {
 
   const elemContainer = document.createElement('div');
 
@@ -902,58 +979,7 @@ function getVolunteerStats(aDocs) {
     oTotals.fHours.toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 }),
     oTotals.fMiles.toLocaleString(undefined, { maximumFractionDigits: 0 })));
   elemTable.appendChild(elemTFoot);
-  // add to the container
-  elemContainer.appendChild(elemTable);
-
-  // SHOW RECENT DOCS
-  // =====================================
-
-  // add a title
-  elemHeading = document.createElement('H4');
-  elemHeading.classList.add("ml-2", "mt-4", "text-center");
-  elemHeading.innerText = "My Recent Docs";
-  elemContainer.appendChild(elemHeading);
-
-  // filter for service date in last 90 days
-  const dt90DaysAgo = new Date(new Date() - 90 * MILLISEC_IN_A_DAY);
-  const aDocs90Days = aDocs.filter(oDoc => dt90DaysAgo <= oDoc["Date of service"]);
-
-  // sort assignments from last 90 days by date
-  aDocs90Days.sort((oDoc1, oDoc2) => {
-    if (oDoc1["Date of service"] < oDoc2["Date of service"])
-      return -1;
-    if (oDoc2["Date of service"] < oDoc1["Date of service"])
-      return 1;
-    return 0;
-  });
-
-  // create the table to display the assignments
-  elemTable = document.createElement('table');
-  elemTable.classList.add("table", "table-sm", "table-striped");
-  // thead
-  elemTHead = document.createElement('thead');
-  elemTHead.classList.add("bg-info", "text-light");
-  elemTHead.appendChild(makeRowHeading("Svc Dt", "Patient", "Care Plan", "Service", "Hrs", "Miles", "Calls"));
-  elemTable.appendChild(elemTHead);
-  // tbody
-  elemTBody = document.createElement('tbody');
-  // rows
-  for (const oDoc of aDocs90Days) {
-    let elemRow = makeRow(
-      getDateOnly(oDoc["Date of service"]),
-      oDoc["Patient name"],
-      oDoc["Care Plan"],
-      oDoc["What did you do?"],
-      getDurationHours(new Date(oDoc["Total time spent"])),
-      oDoc.Mileage,
-      oDoc["Number of calls, if any"],
-    );
-    elemTBody.appendChild(elemRow);
-    elemRow = makeSpanningRow(oDoc["Notes"], 7);
-    elemTBody.appendChild(elemRow);
-  }
-  elemTable.appendChild(elemTBody);
-  // add to the container
+  // add table to the container
   elemContainer.appendChild(elemTable);
 
   return elemContainer;
@@ -968,8 +994,10 @@ function onMenuVolunteerStats() {
   changeMenuAndContentArea("nav--volunteer-stats", gelemContentVolunteerStats);
 
   // convenience copy of the span to place list of open assignments
-  const elemContainer = document.getElementById("list-volunteer-stats-annual");
-  elemContainer.innerText = ""; // clear it from last rendering
+  const elemContainerAnnual = document.getElementById("list-volunteer-stats-annual");
+  const elemContainerDocs = document.getElementById("list-volunteer-stats-docs");
+  elemContainerAnnual.innerText = ""; // clear it from last rendering
+  elemContainerDocs.innerText = ""; // clear it from last rendering
 
   // Unhide loading spinner
   document.querySelector("#content--volunteer-stats .spinner").removeAttribute("hidden");
@@ -996,7 +1024,8 @@ function onMenuVolunteerStats() {
         // render the volunteer assignments content area
         // renderOpenAssignments(elemOpenAssignments);
         // addOpenAssignments(elemContainer);
-        elemContainer.appendChild(getVolunteerStats(aVolunteerDocs));
+        elemContainerAnnual.appendChild(getVolunteerStatsAnnual(aVolunteerDocs));
+        elemContainerDocs.appendChild(getVolunteerStatsDocs(aVolunteerDocs));
       // no assigments for volunteer, unhide message to that effect
       } else {
         document.getElementById("volunteer-has-no-docs").removeAttribute("hidden");
@@ -1012,7 +1041,7 @@ function onMenuVolunteerStats() {
       console.log(`${error}`);
       const sErrorMsg = JSON.stringify(error);
       console.log(sErrorMsg);
-      elemContainer.innerText = sErrorMsg;
+      elemContainerAnnual.innerText = sErrorMsg;
       debugger;
     }); // catch
 }
